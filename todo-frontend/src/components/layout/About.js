443e0d53137ba45c8b4135d0d11d56d4d20d94f0
Premsai Kothuru <pkothuru@isimtl.local>
2020-07-08 14:34:03 -0400

    files

---
+++ todo-frontend/src/components/layout/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+ class About extends React.Component {
+  render() {
+    return (
+      
+        <div className="aboutContainer">
+        <p>This Web app is a todo list</p>
+        </div>  
+        
+    );
+  }
+}
+export default About;
